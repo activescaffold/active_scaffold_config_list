ffe216e96710c67c015c5ecddc7cfc2d83ad200d
Sergio Cambra <sergio@enpijama.es>
2024-04-05 17:45:57 +0200

    bump to 3.5.0

--- lib/active_scaffold_config_list/version.rb
+++ lib/active_scaffold_config_list/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldConfigList
   module Version
     MAJOR = 3
-    MINOR = 4
-    PATCH = 4
+    MINOR = 5
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
